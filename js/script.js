7fe48ce8e0e9270fb907109372576ca065753c28
eli2722 <eli2722@hotmail.com>
2023-11-16 15:13:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function cambiarfondo() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("div_contenedor_ppal").style.backgroundColor = "black";
+    document.getElementById("letra_inclusiva").style.color = "white";
+}
+
+function verinfo(){
+    alert("Eliana Archilla - eli2722@hotmail.com - 2915010483")   
+}
